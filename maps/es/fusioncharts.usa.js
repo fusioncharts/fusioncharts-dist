16e465aaac1d478ea9d956b51cf57f58310b47ce
Rohit Kumar <rohit.kr001@gmail.com>
2020-03-17 02:10:12 +0530

    Updated files for 3.15.1-sr.1 release

--- maps/es/fusioncharts.usa.js
+++ maps/es/fusioncharts.usa.js
@@ -1,9 +1,9 @@
 /**!
  * @license FusionCharts JavaScript Library
- * Copyright InfoSoft Global Pvt. Ltd.
+ * Copyright FusionCharts, Inc.
  * License Information at <http://www.fusioncharts.com/license>
  *
- * @author InfoSoft Global Pvt. Ltd.
+ * @author FusionCharts, Inc.
  * @meta package_map_pack
  * @id fusionmaps.USA.20.10-31-2012 11:18:04
  */
--- maps/es/fusioncharts.world.js
+++ maps/es/fusioncharts.world.js
@@ -1,9 +1,9 @@
 /**!
  * @license FusionCharts JavaScript Library
- * Copyright InfoSoft Global Pvt. Ltd.
+ * Copyright FusionCharts, Inc.
  * License Information at <http://www.fusioncharts.com/license>
  *
- * @author InfoSoft Global Pvt. Ltd.
+ * @author FusionCharts, Inc.
  * @meta package_map_pack
  * @id fusionmaps.World.20.10-30-2012 07:55:31
  */
